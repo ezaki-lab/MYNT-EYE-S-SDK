5fd9a34d6154278f20a56bc595d0639ef178715e
Kaito Hattori <kaichan394@gmail.com>
2020-10-20 18:56:35 +0900

    changed get_disparity.cc

--- samples/get_disparity.cc
+++ samples/get_disparity.cc
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <opencv2/highgui/highgui.hpp>
